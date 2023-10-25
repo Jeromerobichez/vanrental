5d1bcd27feb2db3e421ef65d984979974c0bb4cf
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-10-25 22:36:57 +0200

    lien avec repo github

---
+++ van_rental/Controllers/vanRentalController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vanRental.Models;
+using vanRental.Services;
+
+namespace vanRental.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class vanRentalController : ControllerBase
+    {
+
+        private readonly vanRentalContext _context;
+        private readonly vanRentalService _vanRentalService;
+
+        public vanRentalController(vanRentalContext context, vanRentalService vanRentalService)
+        {
+            _context = context;
+            _vanRentalService = vanRentalService;
+        }
+
+        [HttpGet("test", Name = "test")]
+        public IActionResult Get()
+        {
+           
+            return Ok("Ceci est une réponse de l'API.");
+        }
+        [HttpGet("GetKm", Name = "GetKm")]
+        public async Task<IActionResult> GetKm()
+        {
+            var vehicles = await _context.Vehicles.Select(x => x.Km).ToListAsync();
+           
+            return Ok(vehicles);
+        }
+        [HttpGet("GetAllVehicles", Name = "GetAllVehicles")]
+        public IActionResult GetAllVehicles()
+        {
+            var vehicles = _vanRentalService.GetDataOfVehicles();
+            return Ok(vehicles);
+        }
+    }
+}
---
+++ van_rental/Services/vanRentalService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using vanRental.Models;
+using vanRental.Controllers;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace vanRental.Services
+{
+    public class vanRentalService
+    {
+        private readonly vanRentalContext _context;
+        public vanRentalService(vanRentalContext context)
+        {
+            _context = context;
+        }
+        public List<Vehicles> GetDataOfVehicles()
+        {
+            try
+            {
+                var vehicles =  _context.Vehicles.ToList();
+               
+                return vehicles;
+            }
+            catch (Exception ex)
+            {
+               throw new Exception(ex.Message);
+}
+        }
+    }
+
+    
+
+}

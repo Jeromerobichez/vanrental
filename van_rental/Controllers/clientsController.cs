96e01ba22ef1830e4265ffcd18bf7c3509783c87
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-11-08 11:32:54 +0100

    Création de la route de récupération de tous les clients

---
+++ van_rental/Controllers/clientsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using van_rental.Models;
+using vanRental.Services;
+
+
+namespace van_rental.clientsControllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class clientsController : ControllerBase
+    {
+        private readonly van_rentalContext _context;
+        private readonly vanRentalService _vanRentalService;
+
+        public clientsController(van_rentalContext context, vanRentalService vanRentalService)
+        {
+            _context = context;
+            _vanRentalService = vanRentalService;
+        }
+        [HttpGet("GetAllClients")]
+        public IActionResult GetAllClients()
+        {
+          var allClients = _context.Clients.ToList();
+
+            return Ok(allClients);
+        }
+    }
+
+}
